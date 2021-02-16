0b60a660ec0b23f5126ae196e8020254c627e5c1
kevinpony10 <homerinjuan@hotmail.com>
2021-02-16 14:18:53 -0500

    v2.1

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,4 @@
 
 module.exports = {
-	accountName: 'gamesandbets02',
-	password: 'v1ramses$',
-	sharedSecret: '+VSFhZ/V7sxt4Q4dInwWIpbQR1M=',
-	identitySecret: 'to5bid5pe6BbXpAlBIBTrx2mwT0=',
-	apiKey: '0D54C1C4829E0E28B063C310B8E8DC4E',
-	id:'76561198982979277'
+	
 }
--- src/config/keys.js
+++ src/config/keys.js
@@ -1,8 +1,3 @@
 module.exports = {
-    database:{
-        host: 'bm929hq3ftodrajyo8ou-mysql.services.clever-cloud.com',
-        user: 'uyhjxnoxq2r2puzl',
-        password: 'cC9pquVHm5PPNkZMcc33',
-        database: 'bm929hq3ftodrajyo8ou'
-    }
+    
 }
